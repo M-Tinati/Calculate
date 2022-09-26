bfb015d67fd610f7a992dcd738484c36ecfea933
M-Tinati <114297179+M-Tinati@users.noreply.github.com>
2022-09-26 13:10:20 +0330

    Create Calculate.py

---
+++ Calculate.py
@@ -0,0 +1,7 @@
+number = int(input('your number:'))
+number2 = int(input('your secend number:'))
+operation = input('select operation: + , - ,  ')
+if operation == '+':
+    print(number + number2)
+elif operation == '-':
+    print(number - number2)
